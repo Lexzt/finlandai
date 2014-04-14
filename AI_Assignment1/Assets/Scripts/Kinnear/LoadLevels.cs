14059c9121d60db90c4b224be6eb5cdb528915cf
fongzhizhong@gmail.com <fongzhizhong@gmail.com@244b3012-20aa-7a06-d8d3-3e63c754854c>
2014-04-14 16:06:33 +0000

    Major Changes, Map Editor, AI Pathfinding, First person movement

---
+++ AI_Assignment1/Assets/Scripts/Kinnear/LoadLevels.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LoadLevels : MonoBehaviour {
+
+	// this class for loading and storing of textfiles
+	public TextAsset [] collectiveLevels;
+	public List<TextAsset> waypoints;
+	public List<TextAsset> levels;
+
+
+	public string waypointStringExtension = "PatrolWaypoints";
+
+	public Vector2 scrollPosition = Vector2.zero;
+	
+
+
+	// Use this for initialization
+	void Awake () {
+	}
+
+	public void DeleteLevelsFromMemory()
+	{
+		levels.Clear();
+		waypoints.Clear();
+		System.Array.Clear(collectiveLevels, 0, collectiveLevels.Length);
+	}
+
+	public void LoadLevelsIntoMemory()
+	{
+		collectiveLevels = Resources.LoadAll <TextAsset>("");
+
+		// split text files into waypoint and levels list
+		for(int i = 0; i < collectiveLevels.Length; i++)
+		{
+			if(collectiveLevels[i].name.Contains(waypointStringExtension))
+			{
+				waypoints.Add(collectiveLevels[i]);
+			}
+			else
+			{
+				levels.Add(collectiveLevels[i]);
+			}
+		}
+	}
+}
