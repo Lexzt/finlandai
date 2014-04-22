c2d41a49a9a7afcc8c53a3bad33cb1c74cdbc1fc
vivifira@gmail.com <vivifira@gmail.com@244b3012-20aa-7a06-d8d3-3e63c754854c>
2014-04-22 11:41:12 +0000

    Final Level Editor

---
+++ AI_Assignment1/Assets/Scripts/Kinnear/DrawLines.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DrawLines : MonoBehaviour {
+
+	static Material lineMaterial;
+	static void CreateLineMaterial() {
+		if( !lineMaterial ) {
+			lineMaterial = new Material( "Shader \"Lines/Colored Blended\" {" +
+			                            "SubShader { Pass { " +
+			                            "    Blend SrcAlpha OneMinusSrcAlpha " +
+			                            "    ZWrite Off Cull Off Fog { Mode Off } " +
+			                            "    BindChannels {" +
+			                            "      Bind \"vertex\", vertex Bind \"color\", color }" +
+			                            "} } }" );
+			lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+			lineMaterial.shader.hideFlags = HideFlags.HideAndDontSave;
+		}
+	}
+
+
+	void DrawQuad(Vector3 position, float size, float height)
+	{
+		GL.Begin (GL.QUADS);
+			GL.Vertex3 (position.x - size, height, position.z - size);
+			GL.Vertex3 (position.x - size, height, position.z + size);
+			GL.Vertex3 (position.x + size, height, position.z + size);
+			GL.Vertex3 (position.x + size, height, position.z - size);
+		GL.End ();
+	}
+
+
+	// Draws the lines of the waypoints on render
+	void OnPostRender() 
+	{
+		List<Vector3> lines = GameObject.Find("GameManager").GetComponent<EditLevel>().lineConnector;
+
+		Debug.Log ("Amount of items in list: " + lines.Count);
+
+		CreateLineMaterial();
+		// set the current material
+		lineMaterial.SetPass( 0 );
+
+		GL.Begin( GL.LINES );
+		GL.Color(new Color(1f,1f,1f,1f));
+
+		for(int i = 0; i < lines.Count; i++)
+		{
+			GL.Vertex3( lines[i].x, lines[i].y, lines[i].z );
+
+			if(i > 1)
+			{
+				GL.Vertex3( lines[i - 1].x, lines[i - 1].y, lines[i - 1].z );
+			}
+		}
+
+		GL.End();		
+
+
+		// Draw the waypoin quads
+		for(int i = 0; i < lines.Count; i++)
+		{
+			DrawQuad (lines[i], 0.2f, 0.5f);
+		}
+
+	}
+}
--- AI_Assignment1/Assets/Scripts/Kinnear/EditLevel.cs
+++ AI_Assignment1/Assets/Scripts/Kinnear/EditLevel.cs
@@ -12,6 +12,10 @@ public class EditLevel : MonoBehaviour {
 	// .txt file to load you level from
 	public string levelName = "Assets/Resources/Level1.txt";
 
+	private string waypointStringDifference = "PatrolWaypoints";
+
+	public string waypointName;
+
 	// our selection according to the user
 	public int userSelection = 2;
 
@@ -30,8 +34,97 @@ public class EditLevel : MonoBehaviour {
 
 	private GameObject previousNode;
 
+	public int playerPrefabNumber = 4;
+	public int enemyPrefabNumber = 5;
+
+	// Line positions for waypoints
+	public List<Vector3> lineConnector = new List<Vector3> ();
+
+	// stores the new level's data if theres such a thing
+	public List<List<int>> newLevelNodes = new List<List<int>> ();
+
+	
+	public void CreateNewLevel(string newMapName, int newTerrainWidth, int newTerrainHeight)
+	{
+		newLevelNodes.Clear ();
+
+		string newWaypointName = newMapName;
+		newWaypointName = newWaypointName.Insert (newWaypointName.Length - 4, waypointStringDifference);
+
+		// Initialise the list of new level nodes
+		for(int i = 0; i < newTerrainWidth; i++)
+		{
+			List<int> row = new List<int>();
+
+			for(int j = 0; j < newTerrainHeight; j++)
+			{
+				row.Add(0);
+			}	
+			newLevelNodes.Add(row);
+		}
+
+		string saveText = "";
+		
+		// Write the string to a file.
+		System.IO.StreamWriter file = new System.IO.StreamWriter(newMapName);
+		
+		// concentanate the entire string into one long ass line
+		for(int i = 0; i < newLevelNodes.Count; i++)
+		{
+			for(int j = 0; j < newLevelNodes[0].Count; j++)
+			{
+				saveText += newLevelNodes[i][j];
+				
+				if(j != newLevelNodes.Count - 1)
+				{
+					saveText += " ";
+				}
+			}
+			
+			if(i != newLevelNodes.Count - 1)
+			{
+				saveText += "\n";
+			}
+		}
+		
+		file.WriteLine(saveText);
+		
+		file.Close();
 
 
+		saveText = "";
+
+		// Write the string to the waypoint file
+		System.IO.StreamWriter waypointFile = new System.IO.StreamWriter(newWaypointName);
+		
+		// concentanate the entire string into one long ass line
+		for(int i = 0; i < newLevelNodes.Count; i++)
+		{
+			for(int j = 0; j < newLevelNodes[0].Count; j++)
+			{
+				saveText += newLevelNodes[i][j];
+				
+				if(j != newLevelNodes.Count - 1)
+				{
+					saveText += " ";
+				}
+			}
+			
+			if(i != newLevelNodes.Count - 1)
+			{
+				saveText += "\n";
+			}
+		}
+		
+		waypointFile.WriteLine(saveText);
+		
+		waypointFile.Close();
+	}
+
+
+
+	
+	
 	// Reading from text file, through " " delim.
 	string[][] readFile(string file)
 	{
@@ -90,10 +183,47 @@ public class EditLevel : MonoBehaviour {
 		file.Close();
 	}
 
+	void SaveWaypointFile()
+	{
+		string saveText = "";
+		
+		// Write the string to a file.
+		System.IO.StreamWriter file = new System.IO.StreamWriter(waypointName);
+		
+		// concentanate the entire string into one long ass line
+		for(int i = 0; i < levelNodes.Count; i++)
+		{
+			for(int j = 0; j < levelNodes[0].Count; j++)
+			{
+				saveText += (levelNodes[i][j].GetComponent<NodeDetails>().waypointID).ToString();
+				
+				if(j != levelNodes.Count - 1)
+				{
+					saveText += " ";
+				}
+			}
+			
+			if(i != levelNodes.Count - 1)
+			{
+				saveText += "\n";
+			}
+		}
+		
+		if(saveText.EndsWith("\n"))
+		{
+			Debug.Log("There is a carriage return");
+		}
+		
+		file.WriteLine(saveText);
+		
+		file.Close();
+	}
+
 	public void SaveLevel()
 	{
 		ClearTextFile(levelName);
 		SaveFile();
+		SaveWaypointFile ();
 	}
 
 	public void LoadChoosenLevel()
@@ -108,12 +238,18 @@ public class EditLevel : MonoBehaviour {
 			}
 		}
 
-
 		levelNodes.Clear ();
 
+
+		
 		// read the text file to check what is the height and length
 		string[][] jagged = readFile(levelName);
-		
+
+		waypointName = levelName;
+		waypointName = waypointName.Insert (waypointName.Length - 4, waypointStringDifference);
+
+		string[][] jaggedWaypoints = readFile (waypointName);
+
 		Debug.Log (jagged.Length);
 		Debug.Log (jagged[0].Length);
 		
@@ -123,6 +259,9 @@ public class EditLevel : MonoBehaviour {
 		
 		Debug.Log("Terrain Height: " + terrainHeight);
 		Debug.Log("Terrain Length: " + terrainLength);
+
+		Debug.Log("Waypoint Height: " + jaggedWaypoints[0].Length);
+		Debug.Log("Waypoint Length: " + jaggedWaypoints.Length);
 		
 		
 		for (int y = 0; y < jagged.Length - 1; y++)
@@ -140,6 +279,7 @@ public class EditLevel : MonoBehaviour {
 				assignedNode.GetComponent<NodeDetails>().id = int.Parse(jagged[y][x]);
 				assignedNode.GetComponent<NodeDetails>().x = x;
 				assignedNode.GetComponent<NodeDetails>().y = y;
+				assignedNode.GetComponent<NodeDetails>().waypointID = int.Parse(jaggedWaypoints[y][x]); // assign the waypoint to the node
 				assignedNode.GetComponent<NodeDetails>().SetTypeOfPrefab();
 				
 				// Finally store the node
@@ -148,14 +288,287 @@ public class EditLevel : MonoBehaviour {
 			
 			levelNodes.Add(firstDimensionArray);
 		}
+
+		// set terrain variables here
+		// Terrain height has to minus one because of the EXTRA LINE SPACE IN THE TEXT FILE!
+		GameObject.Find ("Terrain").GetComponent<Terrain> ().terrainData.size = new Vector3 ((float)terrainLength, 1.0f, (float)terrainHeight - 1);
+	}
+
+
+
+	// Checks if the last checkpoint is valid for the user to place for the AI to move from the previous point
+	int CheckIfWaypointIsValid(int x, int y)
+	{
+		int latestWaypoint = 0;
+		int enemyStartX = 0;
+		int enemyStartY = 0;
+
+		// list of passable tiles the Ai can walk through
+		List<int> passableTiles = new List<int>();
+		passableTiles.Add (0);
+		passableTiles.Add (4);
+		passableTiles.Add (5);
+		passableTiles.Add (6);
+
+		for (int i = 0; i < levelNodes.Count; i++)
+		{
+			for (int j = 0; j < levelNodes[0].Count; j++)
+			{
+				// first we check what is the latest waypoint we have specified in the list
+				if(levelNodes[i][j].GetComponent<NodeDetails>().waypointID > latestWaypoint)
+				{
+					// save the latest waypoint's position into memory
+					latestWaypoint = levelNodes[i][j].GetComponent<NodeDetails>().waypointID;
+					enemyStartX = j;
+					enemyStartY = i;
+				}
+			}
+		}
+		Debug.Log ("Latest Waypoint in the CSV: " + latestWaypoint);
+
+		if(levelNodes[y][x].GetComponent<NodeDetails>().waypointID == 0)
+		{
+
+		// this means that we have not specified a waypoint before [:
+		if(latestWaypoint == 0)
+		{
+			// obtain the location of the enemy spawn point
+			for (int i = 0; i < levelNodes.Count; i++)
+			{
+				for (int j = 0; j < levelNodes[0].Count; j++)
+				{
+					// we check where is the enemy spawn point and we obtain x and y coordinates
+					if(levelNodes[i][j].GetComponent<NodeDetails>().id == enemyPrefabNumber)
+					{
+						enemyStartX = levelNodes[i][j].GetComponent<NodeDetails>().x;
+						enemyStartY = levelNodes[i][j].GetComponent<NodeDetails>().y;
+						break;
+					}
+				}
+			}
+			
+			//compare if the waypoint declared by the user is possible to move to from the enemy location on the map
+			// First we compare to see if the row or column that the user has picked is within the same row or column
+			if(x == enemyStartX|| y == enemyStartY)
+			{
+				// Next we check if theres anything inbetween the two nodes that we cannot walk through
+				if(x == enemyStartX)
+				{
+					// We can confirm that the user has clicked on the left side of the starting node
+					if(y < enemyStartY)
+					{
+						// Check everything between both nodes if there are any nodes that are impassable
+						for(int i = y; i < enemyStartY; i++)
+						{
+							if(!passableTiles.Contains(levelNodes[i][x].GetComponent<NodeDetails>().id))
+							{
+								return 0;
+							}
+						}
+					}
+					else
+					{
+						// Check everything between both nodes if there are any nodes that are impassable
+						for(int i = enemyStartY; i <= y; i++)
+						{
+							if(!passableTiles.Contains(levelNodes[i][x].GetComponent<NodeDetails>().id))
+							{
+								return 0;
+							}
+						}
+					}
+
+				}
+				else if(y == enemyStartY)
+				{
+					// We can confirm that the user has clicked on the left side of the starting node
+					if(x < enemyStartX)
+					{
+						// Check everything between both nodes if there are any nodes that are impassable
+						for(int i = x; i < enemyStartX; i++)
+						{
+							if(!passableTiles.Contains(levelNodes[y][i].GetComponent<NodeDetails>().id))
+							{
+								return 0;
+							}
+						}
+					}
+					else
+					{
+						// Check everything between both nodes if there are any nodes that are impassable
+						for(int i = enemyStartX; i <= x; i++)
+						{
+							if(!passableTiles.Contains(levelNodes[y][i].GetComponent<NodeDetails>().id))
+							{
+								return 0;
+							}
+						}
+					}
+				}
+
+				return 1;
+			}
+		}
+		else
+		{
+//			// it's not our first waypoint
+//			// so we take the latest waypoint's node's position and we compare it to our clicked position!
+//			// compare if the waypoint declared by the user is possible to move to from the enemy location on the map
+//			// First we compare to see if the row or column that the user has picked is within the same row or column
+
+			// The user has selected another waypoint
+			if(x == enemyStartX && y == enemyStartY)
+			{
+				// remove that waypoint and another other waypoint numbers that are after this waypoint
+				//UnselectedWaypoint(x, y);
+				return 0;
+			}
+			else if(x == enemyStartX || y == enemyStartY)
+			{
+				// Next we check if theres anything inbetween the two nodes that we cannot walk through
+				if(x == enemyStartX)
+				{
+					// We can confirm that the user has clicked on the left side of the starting node
+					if(y < enemyStartY)
+					{
+						// Check everything between both nodes if there are any nodes that are impassable
+						for(int i = y; i < enemyStartY; i++)
+						{
+							if(!passableTiles.Contains(levelNodes[i][x].GetComponent<NodeDetails>().id))
+							{
+								return 0;
+							}
+						}
+					}
+					else
+					{
+						// Check everything between both nodes if there are any nodes that are impassable
+						for(int i = enemyStartY; i <= y; i++)
+						{
+							if(!passableTiles.Contains(levelNodes[i][x].GetComponent<NodeDetails>().id))
+							{
+								return 0;
+							}
+						}
+					}
+					
+				}
+				else if(y == enemyStartY)
+				{
+					// We can confirm that the user has clicked on the left side of the starting node
+					if(x < enemyStartX)
+					{
+						// Check everything between both nodes if there are any nodes that are impassable
+						for(int i = x; i < enemyStartX; i++)
+						{
+							if(!passableTiles.Contains(levelNodes[y][i].GetComponent<NodeDetails>().id))
+							{
+								return 0;
+							}
+						}
+					}
+					else
+					{
+						// Check everything between both nodes if there are any nodes that are impassable
+						for(int i = enemyStartX; i <= x; i++)
+						{
+							if(!passableTiles.Contains(levelNodes[y][i].GetComponent<NodeDetails>().id))
+							{
+								return 0;
+							}
+						}
+					}
+				}
+				
+					return latestWaypoint + 1;
+				}
+
+			}
+		}
+		// this means that the user is attempting to delete a waypoint
+		else
+		{
+			UnselectedWaypoint(x, y);
+		}
+
+		return 0;
+	}
+
+	// remove all waypoints that are after this node's waypoint ID
+	void UnselectedWaypoint(int x, int y)
+	{
+		int removeFromWaypointID = levelNodes [y] [x].GetComponent<NodeDetails> ().waypointID;
+		Debug.Log ("Remove from CSV any waypoint above: " + removeFromWaypointID);
+
+		// do until the waypoints id in the levelNodes list doesnt increase again
+		for (int i = 0; i < levelNodes.Count; i++)
+		{
+			for (int j = 0; j < levelNodes[0].Count; j++)
+			{
+				// we get the next waypoint in the list (IF THERE SUCH A WAYPOINT HAHA)
+				if(levelNodes[i][j].GetComponent<NodeDetails>().waypointID > removeFromWaypointID)
+				{
+					// set the waypoint value to 0
+					levelNodes[i][j].GetComponent<NodeDetails>().waypointID = 0;
+				}
+			}
+		}
 	}
 
 
 	void Awake()
 	{
+		GameObject.Find ("Terrain").transform.position -= new Vector3 (0.5f, 0.0f, 0.5f);
+		
 		LoadChoosenLevel ();
 	}
 
+	// store the waypoint positions into a List
+	void StoreWaypointLine()
+	{
+		lineConnector.Clear ();
+
+		// obtain the location of the enemy spawn point
+		for (int i = 0; i < levelNodes.Count; i++)
+		{
+			for (int j = 0; j < levelNodes[0].Count; j++)
+			{
+				// we check where is the enemy spawn point and we obtain x and y coordinates
+				if(levelNodes[i][j].GetComponent<NodeDetails>().id == enemyPrefabNumber)
+				{
+					lineConnector.Add(levelNodes[i][j].transform.position + new Vector3(0.0f, 0.5f, 0.0f));
+					break;
+				}
+			}
+		}
+
+		int previousWaypointFigure = 0;
+		int currentWaypointFigure = 0;
+
+		// do until the waypoints id in the levelNodes list doesnt increase again
+		do
+		{
+			previousWaypointFigure++;
+
+			for (int i = 0; i < levelNodes.Count; i++)
+			{
+				for (int j = 0; j < levelNodes[0].Count; j++)
+				{
+					// we get the next waypoint in the list (IF THERE SUCH A WAYPOINT HAHA)
+					if(levelNodes[i][j].GetComponent<NodeDetails>().waypointID == currentWaypointFigure + 1)
+					{
+						// save the latest waypoint's position into memory
+						lineConnector.Add(levelNodes[i][j].transform.position + new Vector3(0.0f, 0.5f, 0.0f));
+						currentWaypointFigure++;
+						break;
+					}
+				}
+			}
+
+		}while(currentWaypointFigure >= previousWaypointFigure);
+
+	}
+	
 	// Update is called once per frame
 	void Update () 
 	{
@@ -203,21 +616,55 @@ public class EditLevel : MonoBehaviour {
 		}
 
 
-		if(Input.GetMouseButton(0))
+		// Click to change a node's prefab
+		if(Input.GetMouseButtonDown(0) && GUIUtility.hotControl == 0)
 		{
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit = new RaycastHit();
 
 			if (Physics.Raycast (ray, out hit))
 			{
-				//Debug.Log("Hit something");
-				
-				// if we hit a transformable gameobject
-				if(hit.collider.transform.gameObject.GetComponent<NodeDetails>() != null)
-				{
-					hit.transform.gameObject.GetComponent<NodeDetails>().id = userSelection;
-					hit.transform.gameObject.GetComponent<NodeDetails>().SetTypeOfPrefab();
-				}
+					// if we have a gameobject that is not null
+					if(hit.collider.transform.gameObject.GetComponent<NodeDetails>() != null)
+					{
+							// if we hit a transformable gameobject and we've not selected a waypoint
+							if(!GetComponent<DisplayEditorGUI>().waypointApplication)
+							{
+								if(userSelection == playerPrefabNumber  || userSelection == enemyPrefabNumber) // this we have specified a player/enemy spawn location we have to remove any other player/enemy start points
+								{
+									for(int i = 0; i < levelNodes.Count; i++)
+									{
+										for(int j = 0; j < levelNodes[0].Count; j++)
+										{
+											if(levelNodes[i][j].GetComponent<NodeDetails>().id == userSelection)
+											{
+												levelNodes[i][j].GetComponent<NodeDetails>().id = 0;
+												levelNodes[i][j].GetComponent<NodeDetails>().SetTypeOfPrefab();
+											}
+										}	
+									}
+								}
+
+								hit.transform.gameObject.GetComponent<NodeDetails>().id = userSelection;
+								hit.transform.gameObject.GetComponent<NodeDetails>().SetTypeOfPrefab();
+
+								// if we accidentally tried to place a block on a waypoint remove the rest of the waypoints!
+								if(hit.transform.gameObject.GetComponent<NodeDetails>().waypointID != 0)
+								{
+									UnselectedWaypoint(hit.transform.gameObject.GetComponent<NodeDetails>().x, hit.transform.gameObject.GetComponent<NodeDetails>().y);
+									hit.transform.gameObject.GetComponent<NodeDetails>().waypointID = 0;
+								}
+							}
+						
+							// We check if the waypoint is valid and then we apply the waypoint indication number and then we set that node in the level to 0 which is clear block
+							else
+							{
+								// calculate if we can set a waypoint here!
+								hit.transform.gameObject.GetComponent<NodeDetails>().waypointID = CheckIfWaypointIsValid(hit.transform.gameObject.GetComponent<NodeDetails>().x, hit.transform.gameObject.GetComponent<NodeDetails>().y);
+								Debug.Log("New Waypoint number is : " + hit.transform.gameObject.GetComponent<NodeDetails>().waypointID);
+							}
+					}
+
 			}
 		}
 
@@ -226,6 +673,7 @@ public class EditLevel : MonoBehaviour {
 			SaveLevel();
 		}
 
+		StoreWaypointLine ();
 	}
 
 }
