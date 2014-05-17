f75a2948601b05fd4b1c863ab27ae0e2a87d65e4
fongzhizhong@gmail.com <fongzhizhong@gmail.com@244b3012-20aa-7a06-d8d3-3e63c754854c>
2014-05-17 09:45:59 +0000

    AI Bug fixes

--- AI_Assignment1/Assets/Scripts/AIAStarScript.cs
+++ AI_Assignment1/Assets/Scripts/AIAStarScript.cs
@@ -19,94 +19,80 @@ public class AIAStarScript : MonoBehaviour {
 	private List<GameObject> ListArray;
 	private GameObject[] FinalArray;
 
+	private GameObject LevelGeneratorClass;
+	private LevelGenerator LevelGeneratorInstance;
+
     void Start ()
     {
+		LevelGeneratorClass = GameObject.FindGameObjectWithTag ("LevelGenerator");
+		LevelGeneratorInstance = LevelGeneratorClass.GetComponent<LevelGenerator>();
+
 		GameObject Newplayer = GameObject.FindGameObjectWithTag("Player");
 		currentTarget = new GameObject();
 		currentTarget.transform.position = new Vector3(Newplayer.transform.position.x, Newplayer.transform.position.y, Newplayer.transform.position.z);
 
-		GameObject[] ObjectArray 	= GameObject.FindGameObjectsWithTag ("Bits");
-		GameObject[] BigBitsArray 	= GameObject.FindGameObjectsWithTag ("BigBits");
-		GameObject[] EmptyArray 	= GameObject.FindGameObjectsWithTag ("Empty");
-		GameObject EnemySpawn 		= GameObject.FindGameObjectWithTag 	("EnemySpawn");
-		GameObject PlayerSpawn	 	= GameObject.FindGameObjectWithTag 	("PlayerSpawn");
-		ListArray = new List<GameObject>();
-		
-		foreach (GameObject v in ObjectArray) 
-		{
-			ListArray.Add(v);
-		}
-		
-		foreach (GameObject v in BigBitsArray) 
-		{
-			ListArray.Add(v);
-		}
-		
-		foreach (GameObject v in EmptyArray) 
-		{
-			ListArray.Add(v);
-		}		
-		ListArray.Add(EnemySpawn);
-		ListArray.Add(PlayerSpawn);
-		
-		FinalArray = ListArray.ToArray ();
+        FinalArray = LevelGeneratorInstance.CurrentActiveLevel();
     }
 	
 	void Update () 
 	{
 		GameObject player = GameObject.FindGameObjectWithTag("Player");
+
 		
-		if (currentTarget.transform.position.x != player.transform.position.x &&
-		    currentTarget.transform.position.z != player.transform.position.z)
+		if(player.GetComponent<PlayerScript>().invulnerable == false)
 		{
-			m_bReCalc = true;
-		}
-		else
-		{
-			m_bReCalc = false;
-		}
-		
-		if(s_Path == null)
-		{
-			s_Path = AStarAlgorithm.AStar(	FinalArray,
-											gameObject.GetComponent<CurrentNodeScript>().currentNode,
-											player.GetComponent<CurrentNodeScript>().currentNode);
-		}
-		else if (m_bReCalc == true)
-		{
-			m_bReCalc = false;
-			currentTarget.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
-			
-			s_Path.Clear();
-			s_Path = AStarAlgorithm.AStar(	FinalArray,
-											gameObject.GetComponent<CurrentNodeScript>().currentNode,
-											player.GetComponent<CurrentNodeScript>().currentNode);
-		}
-		
-		if (gameObject.GetComponent<CurrentNodeScript>().currentNode.transform.position != player.GetComponent<CurrentNodeScript>().currentNode.transform.position)
-		{
-			if (s_Path != null)
+			if (currentTarget.transform.position.x != player.transform.position.x &&
+			    currentTarget.transform.position.z != player.transform.position.z)
 			{
-				if ((currentEnd == null || transform.position == currentEnd.transform.position) && s_Path.Count != 0)
+				m_bReCalc = true;
+			}
+			else
+			{
+				m_bReCalc = false;
+			}
+			
+			if(s_Path == null)
+			{
+				s_Path = AStarAlgorithm.AStar(	FinalArray,
+												gameObject.GetComponent<CurrentNodeScript>().currentNode,
+												player.GetComponent<CurrentNodeScript>().currentNode);
+			}
+			else if (m_bReCalc == true)
+			{
+				m_bReCalc = false;
+				currentTarget.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+				
+				s_Path.Clear();
+				s_Path = AStarAlgorithm.AStar(	FinalArray,
+												gameObject.GetComponent<CurrentNodeScript>().currentNode,
+												player.GetComponent<CurrentNodeScript>().currentNode);
+			}
+			
+			if (gameObject.GetComponent<CurrentNodeScript>().currentNode.transform.position != player.GetComponent<CurrentNodeScript>().currentNode.transform.position)
+			{
+				if (s_Path != null)
 				{
-					currentEnd = s_Path.Pop();
-					
-					startTime = Time.time;
-					journeyLength = Vector3.Distance(transform.position, currentEnd.transform.position);
-				}
-				else
-				{
-					float distCovered = (Time.time - startTime) * m_fSpeed;
-					float fracJourney = distCovered / journeyLength;
-					
-					transform.position = Vector3.MoveTowards(transform.position, currentEnd.transform.position, fracJourney);
+					if ((currentEnd == null || transform.position == currentEnd.transform.position) && s_Path.Count != 0)
+					{
+						currentEnd = s_Path.Pop();
+						
+						startTime = Time.time;
+						journeyLength = Vector3.Distance(transform.position, currentEnd.transform.position);
+					}
+					else
+					{
+						float distCovered = (Time.time - startTime) * m_fSpeed;
+						float fracJourney = distCovered / journeyLength;
+						
+						transform.position = Vector3.MoveTowards(transform.position, currentEnd.transform.position, fracJourney);
+					}
 				}
 			}
-		}
-		else
-		{
-			// Player minus health over here.
-			// Add later
+			else
+			{
+				// Player minus health over here.
+				// Add later
+			}
 		}
 	}
 }
